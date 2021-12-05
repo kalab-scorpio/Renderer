f3162ccd0d0d48f792add3d8a5519eb2a32a07ce
kalab <kalab@pop-os.localdomain>
2021-12-05 21:47:42 +0300

    updated renderer to handle clear with color

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -16,6 +16,11 @@ void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader, in
     glDrawElementsInstanced(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, 0,primCount);
 }
 
+void Renderer::Clear(float r, float g, float b, float a)const{
+    glClearColor(r, g, b, a);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
 void Renderer::Clear() const{
     // glClearColor(0.0f, 0.3f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
